3af4c3262459f0dd15c2290dd20349a3c58639e0
Aurelie Raout <aurelieraout@gmail.com>
2025-02-12 19:44:09 +0100

    views index and show in progress

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "kamal", require: false
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
 
+gem "simple_form"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -47,6 +49,8 @@ group :development, :test do
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
 end
+
+
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.destroy_all
+puts "Creating flats..."
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts "Created London"
+
+Flat.create!(
+  name: 'Small Flat Paris',
+  address: '21 rue des Fleurs 75002 Paris',
+  description: 'Nive little flat in Paris',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+puts "Created Paris"
+
+Flat.create!(
+  name: 'Spacious Garden Flat Berlin',
+  address: '10 - Gardens Berlin',
+  description: 'Spacious and cosy flat',
+  price_per_night: 85,
+  number_of_guests: 3
+)
+puts "Created Berlin"
+
+Flat.create!(
+  name: 'Large Flat Manchester',
+  address: '56 Roses Manchester',
+  description: 'Discover the city around',
+  price_per_night: 95,
+  number_of_guests: 6
+)
+puts "Created Manchester"
+
+Flat.create!(
+  name: 'Wonderful Flat in Barcelona',
+  address: '34 La Barceloneta, Barcelona',
+  description: 'Near the beach and nice restaurants',
+  price_per_night: 55,
+  number_of_guests: 4
+)
+puts "Created Barcelona"
+puts "Finished! Created #{Flat.count} flats."
